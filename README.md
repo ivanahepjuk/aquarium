07aaa8ebbc3f85bd325effd5017edfdfb1938e4a
ivanahepjuk <ivanahepjuk@gmail.com>
2016-08-28 10:28:26 +0000

    streaming added

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# aquarium
+This initial code is based on two resources:
 
-# Web server created for stalking my aquarium why I am not at home.
+1. http://randomnerdtutorials.com/raspberry-pi-web-server-using-flask-to-control-gpios/  by Rui Santos
+2.article on [video streaming with Flask](http://blog.miguelgrinberg.com/post/video-streaming-with-flask) by Miguel Grinberg
+
+Requirements:
+
+Flask==0.10.1
+Jinja2==2.7.3
+MarkupSafe==0.23
+Werkzeug==0.9.6
+itsdangerous==0.24
--- app.py
+++ app.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python
 '''
-
-Adapted excerpt from Getting Started with Raspberry Pi by Matt Richardson
-
-Modified by Rui Santos
-Complete project details: http://randomnerdtutorials.com
+contributions
 
 '''
-
 import RPi.GPIO as GPIO
-from flask import Flask, render_template, request
-app = Flask(__name__)
+from flask import Flask, render_template, Response
 
+# Raspberry Pi camera module (requires picamera package)
+from camera_pi import Camera
+
+app = Flask(__name__)
+#setting gpio into the initial mode
 GPIO.setmode(GPIO.BCM)
+
 
 # Create a dictionary called pins to store the pin number, name, and pin state:
 pins = {
@@ -24,17 +25,21 @@ for pin in pins:
    GPIO.setup(pin, GPIO.OUT)
    GPIO.output(pin, GPIO.LOW)
 
-@app.route("/")
-def main():
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
-   templateData = {
-      'pins' : pins
-      }
-   # Pass the template data into the template main.html and return it to the user
-   return render_template('main.html', **templateData)
+@app.route('/')
+def index():
+
+     # For each pin, read the pin state and store it in the pins dictionary:
+     for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+     # Put the pin dictionary into the template data dictionary:
+     templateData = {
+        'pins' : pins
+        }
+     # Pass the template data into the template main.html and return it to the user
+     return render_template('index.html', **templateData)
+
+##//#    """Video streaming home page."""
+##//#    return render_template('index.html')
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
 @app.route("/<changePin>/<action>")
@@ -52,19 +57,32 @@ def action(changePin, action):
    if action == "off":
       GPIO.output(changePin, GPIO.LOW)
       message = "Turned " + deviceName + " off."
-
    # For each pin, read the pin state and store it in the pins dictionary:
    for pin in pins:
       pins[pin]['state'] = GPIO.input(pin)
-
    # Along with the pin dictionary, put the message into the template data dictionary:
    templateData = {
       'pins' : pins
    }
 
-   return render_template('main.html', **templateData)
-
-if __name__ == "__main__":
-   app.run(host='192.168.1.5', port=7777, debug=True)
+   return render_template('index.html', **templateData)
 
 
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.5', port=8000, debug=True, threaded=True)
